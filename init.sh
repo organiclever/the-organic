35d95b6f104e44b3ce8c7dd8bb3e4105eb75bea3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-17 21:07:55 +0700

    init repo-manager cmd

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the repo-manager directory
+cd repo-manager
+
+# Run the Rust program
+cargo run
+
+# Return to the original directory
+cd ..
--- repo-manager/src/main.rs
+++ repo-manager/src/main.rs
@@ -1,3 +1,22 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Repo Manager")
+        .version("1.0")
+        .author("Your Name")
+        .about("Manages repositories")
+        .arg(
+            Arg::with_name("greet")
+                .long("greet")
+                .value_name("NAME")
+                .help("Greets the specified name")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    if let Some(name) = matches.value_of("greet") {
+        println!("Hello {}!", name);
+    } else {
+        println!("Use -h or --help for usage information.");
+    }
 }
