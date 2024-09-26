db79ff1a489c12f7489b5fc1c410d06ad6ab8162
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-26 11:42:39 +0700

    sentinel-web: update type annotations

--- apps/sentinel-web/app/config.py
+++ apps/sentinel-web/app/config.py
@@ -1,24 +1,26 @@
 import json
 from pathlib import Path
-from typing import Dict, Union
-import os
+from typing import Dict, Union, TypedDict, Literal
 
 
-DEFAULT_CONFIG: Dict[str, Union[int, str]] = {"port": 8000, "db_path": "./sentinel.db"}
+class ConfigDict(TypedDict):
+    port: int
+    db_path: str
 
 
-def load_config() -> Dict[str, Union[int, str]]:
+DEFAULT_CONFIG: ConfigDict = {"port": 8000, "db_path": "./sentinel.db"}
+
+
+def load_config() -> ConfigDict:
     config_path: Path = Path(__file__).parent.parent / "config.json"
     try:
         with open(config_path, "r") as config_file:
             loaded_config: Dict[str, Union[int, str]] = json.load(config_file)
-            config = {**DEFAULT_CONFIG, **loaded_config}
+            config: ConfigDict = {**DEFAULT_CONFIG, **loaded_config}  # type: ignore
 
             # Convert relative db_path to absolute path
             if "db_path" in config:
-                db_path = str(config["db_path"])  # Ensure db_path is a string
-                db_path = os.path.expanduser(db_path)
-                db_path = Path(db_path)
+                db_path: Path = Path(config["db_path"]).expanduser()
                 if not db_path.is_absolute():
                     db_path = (config_path.parent / db_path).resolve()
                 config["db_path"] = str(db_path)
@@ -38,6 +40,6 @@ def load_config() -> Dict[str, Union[int, str]]:
         return DEFAULT_CONFIG
 
 
-config: Dict[str, Union[int, str]] = load_config()
-PORT: int = int(config["port"])
-DB_PATH: str = str(config["db_path"])
+config: ConfigDict = load_config()
+PORT: Literal[8000, 8001] = config["port"]  # type: ignore
+DB_PATH: str = config["db_path"]
